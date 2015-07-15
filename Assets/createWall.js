0842c7596181d6a71b170c108f5c7b0640151461
Paul Michael Normington <paul@Pauls-MacBook-Pro.local>
2015-07-15 13:07:51 +0100

    Initial commit

---
+++ Assets/createWall.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+	for (var y = 0; y < 5; y++) {
+        for (var x = 0; x < 5; x++) {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.AddComponent.<Rigidbody>();
+            cube.transform.position = Vector3 (x, y, 0);
+        }
+    }
+}
+
+function Update () {
+
+}
---
+++ Assets/lookAt.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+public var target: Transform;
+
+function Update() {
+	// Rotate the camera every frame so it keeps looking at the target 
+	transform.LookAt(target);
+}
